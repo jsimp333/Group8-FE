7c25473a79301da9fa35fb3ba2d7f39d95a847ad
James Simpson <146882173+jsimp333@users.noreply.github.com>
2024-11-26 14:31:49 -0800

    Update SearchMethod.tsx

--- src/components/SearchMethod.tsx
+++ src/components/SearchMethod.tsx
@@ -1,6 +1,6 @@
 import Avatar from "@mui/material/Avatar";
 
-export default function SearchMethod({ method }: { method: number }) {
+export default function SearchMethod({ method }: { method: number, label: string}) {
   return (
     <Avatar variant="rounded">
       {method}
