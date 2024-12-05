d3d84c57060ff01f380668315deaaea96e2e9a55
Brandon Ragghianti <brandonragghianti@gmail.com>
2024-12-05 01:41:18 -0800

    Centralized theming for view books.

--- src/components/BookView.tsx
+++ src/components/BookView.tsx
@@ -2,6 +2,10 @@ import { Link, ListItemButton, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { IBook } from 'types/books';
 import { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material';
+import Book from '@mui/icons-material/Book';
+
+const defaultTheme = createTheme();
 
 export default function BookView({ book }: { book: IBook }) {
 
@@ -11,52 +15,57 @@ export default function BookView({ book }: { book: IBook }) {
     setImageWidth(event.target.width);
   }
 
+
   return (
-    <Box
-      sx={{
-        marginTop: 4,
-        marginBottom: 4,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}
-    >
-      <ListItemButton
-        component={Link}
-        href={`/books/view?isbn=${book.isbn13}`}
+    <ThemeProvider theme={defaultTheme}>
+      <Box
         sx={{
-          border: 1,
-          borderColor: 'primary.main',
-          borderWidth: 3,
+          marginTop: 4,
+          marginBottom: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
         }}
       >
-        <Box
+        <ListItemButton
+          component={Link}
+          href={`/books/view?isbn=${book.isbn13}`}
           sx={{
-            marginTop: 2,
-            marginBottom: 2,
-            marginLeft: 2,
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            minWidth: 600
+            border: 1,
+            borderColor: 'primary.main',
+            borderWidth: 3,
           }}
         >
-          <img className="view-book-small-image" id="bookItemImage" src={book.icons.large} alt={book.title} onLoad={handleImageLoad}/>
-          {imageWidth && (  // Only render the Box when the image width is available
-          <Box maxWidth={600 - imageWidth}>
-            <Typography className="view-books" variant="h5" sx={{marginLeft: 3}}>
-              Title: {book.title}
-            </Typography>
-            <Typography className="view-books" variant="h5" sx={{marginLeft: 3}}>
-              Author: {book.authors}
-            </Typography>
-            <Typography className="view-books" variant="h5" sx={{marginLeft: 3}}>
-              ISBN: {book.isbn13}
-            </Typography>
+          <Box
+            sx={{
+              marginTop: 2,
+              marginBottom: 2,
+              marginLeft: 1,
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              minWidth: 600
+            }}
+          >
+            <img className="view-book-small-image" id="bookItemImage" src={book.icons.large} alt={book.title} onLoad={handleImageLoad}/>
+            {imageWidth && (  // Only render the Box when the image width is available
+            <Box maxWidth={600 - imageWidth}>
+              <BookItem item={`Title: ${book.title}`} />
+              <BookItem item={`Authors: ${book.authors}`} />
+              <BookItem item={`ISBN: ${book.isbn13}`} />
+            </Box>
+          )}
           </Box>
-        )}
-        </Box>
-      </ListItemButton>
-    </Box>
+        </ListItemButton>
+      </Box>
+    </ThemeProvider>
   );
+
+  function BookItem({ item } : { item: string }) {
+    return (
+      <Typography className="view-books" variant="h6" sx={{marginLeft: 3, fontWeight:"bold"}}>
+        {item}
+      </Typography>
+    );
+  }
 }
